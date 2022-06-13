a0521809e6b6683210b473268be7202aac8c22d6
CUSTOMFAIL <105759853+CUSTOMFAIL@users.noreply.github.com>
2022-06-13 11:10:32 +0530

    Create genra.py

---
+++ zerotwobot/modules/genra.py
@@ -0,0 +1,23 @@
+import os
+import re
+from platform import python_version as kontol
+from telethon import events, Button
+from telegram import __version__ as telever
+from telethon import __version__ as tlhver
+from pyrogram import __version__ as pyrover
+from zerotwobot.events import register
+from zerotwobot import telethn as tbot
+
+
+PHOTO = "https://telegra.ph/file/38513d13b5f772f2a36bd.jpg"
+
+@register(pattern=("/aliv"))
+async def awake(event):
+  TEXT = f"**Hi I'm Officer Jenny.** \n\n"
+  TEXT += "❍ **I'm Working Properly** \n\n"
+  TEXT += f"❍ **My Master : [AYATO](https://t.me/SILVER_KING)** \n\n"
+  TEXT += f"❍ **Library Version :** `{telever}` \n\n"
+  TEXT += f"❍ **Telethon Version :** `{tlhver}` \n\n"
+  TEXT += f"❍ **Pyrogram Version :** `{pyrover}` \n\n"
+  TEXT += "**Thanks For Adding Me Here ❤️**"
+  await tbot.send_file(event.chat_id, PHOTO, caption=TEXT)
