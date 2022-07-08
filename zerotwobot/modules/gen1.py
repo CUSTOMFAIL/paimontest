025cc09ee581cf7c88875df6cb4fe20e21411225
CUSTOMFAIL <105759853+CUSTOMFAIL@users.noreply.github.com>
2022-07-08 18:12:29 +0530

    Update gen1.py

--- zerotwobot/modules/gen1.py
+++ zerotwobot/modules/gen1.py
@@ -14,4 +14,4 @@ async def awake(event):
   TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}).** \n\n"
   TEXT += "❍ **LIST OF POKEMON IN GENERATION 1** \n\n"
   BUTTON = [[Button.url("GEN1", "https://telegra.ph/GEN1-07-08")]]
-  await tbot.send_message(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
+  await tbot.send_message(event.chat_id, caption=TEXT,  buttons=BUTTON)
