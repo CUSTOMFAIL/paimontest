990cbc720dd0bae8a6e4e7d3be043b13ba951bda
CUSTOMFAIL <105759853+CUSTOMFAIL@users.noreply.github.com>
2022-07-08 14:01:12 +0530

    Update gen3.py

--- zerotwobot/modules/gen3.py
+++ zerotwobot/modules/gen3.py
@@ -12,6 +12,6 @@ from zerotwobot import telethn as tbot
 @register(pattern=("/gen3"))
 async def awake(event):
   TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}).** \n\n"
-  TEXT += "❍ **GENERATION 3** \n\n"
+  TEXT += "❍ **LIST OF POKEMON IN GENERATION 3** \n\n"
   BUTTON = [[Button.url("GEN3", "https://telegra.ph/GEN3-07-08")]]
   await tbot.send_message(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
