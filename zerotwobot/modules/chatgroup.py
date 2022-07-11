aea099d4bdef69f1b9c48f230b0d11a854020310
CUSTOMFAIL <105759853+CUSTOMFAIL@users.noreply.github.com>
2022-07-12 01:21:52 +0530

    Create chatgroup.py

---
+++ zerotwobot/modules/chatgroup.py
@@ -0,0 +1,27 @@
+import os
+import re
+from platform import python_version as kontol
+from telethon import events, Button
+from telegram import __version__ as telever
+from telethon import __version__ as tlhver
+from pyrogram import __version__ as pyrover
+from zerotwobot.events import register
+from zerotwobot import telethn as tbot
+
+
+@register(pattern=("/chatgroup"))
+async def awake(event):
+  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}).** \n\n"
+  TEXT += "❍ **HERE ARE 3 CHAT GRPS WHERE U CAN TALK RELATED TO HEXA** \n\n"
+  BUTTON = [
+      [
+          Button.url("CHATTERPOKE", "https://t.me/ChatterPokeandHexa")
+      ],
+      [
+          Button.url("GANGLAND", "https://t.me/killers69")
+      ],
+      [
+          Button.url("AKATSUKI", "https://t.me/HEXA_AKATSUKI")
+      ]
+    ]
+  await tbot.send_message(event.chat_id, TEXT,  buttons=BUTTON)
