527e6a2c126dff5d5e640edb679c433bb1a613ac
CUSTOMFAIL <105759853+CUSTOMFAIL@users.noreply.github.com>
2022-07-11 16:28:29 +0530

    Update and rename Gamma.py to gamma.py

--- zerotwobot/modules/gamma.py
+++ zerotwobot/modules/gamma.py
@@ -15,19 +15,19 @@ PHOTO = "https://telegra.ph/file/23db949e5f1aaf1b44889.jpg"
 async def awake(event):
   TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}).** \n\n"
   TEXT += "❍ **TEAM GAMMA ** \n\n"
-  TEXT += f"❍ **CAPTAIN : [AIZEN](https://t.me/above_all_gods)** \n\n"
-  TEXT += f"❍ **VICE CAP : [EREN](https://t.me/ereh_kun)** \n\n"
+  TEXT += f"❍ **CAPTAIN : [VIKING VECTOR](https://t.me/The_Viking_khilji)** \n\n"
+  TEXT += f"❍ **VICE CAP : [TARUN](https://t.me/NagandlaTarunChowdary)** \n\n"
   TEXT += f"❍ **IF U WANT TO JOIN TEAM GAMMA USE THE BUTTON BELOW\n\n"
   BUTTON = [
       [
-          Button.url("『CAPTAIN』", "https://t.me/above_all_gods"),
-          Button.url("『VICE CAP』", "https://t.me/ereh_kun")
+          Button.url("『CAPTAIN』", "https://t.me/The_Viking_khilji"),
+          Button.url("『VICE CAP』", "https://t.me/NagandlaTarunChowdary")
       ],
       [
-          Button.url("『TEAM GAMMA』", "https://t.me/theta_clique")
+          Button.url("『TEAM GAMMA』", "https://t.me/GammaFamily")
       ],
       [
-          Button.url("『 HOW TO JOIN GAMMA 』", "https://t.me/ThetaRecruitment_bot")
+          Button.url("『 HOW TO JOIN GAMMA 』", "https://t.me/GammaRegistration_bot")
       ]
     ]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
