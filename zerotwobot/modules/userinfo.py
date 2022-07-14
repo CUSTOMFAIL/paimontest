9f783985608d1e09fb77825f74f2743d4a6f3077
CUSTOMFAIL <105759853+CUSTOMFAIL@users.noreply.github.com>
2022-07-14 11:01:16 +0530

    Update userinfo.py

--- zerotwobot/modules/userinfo.py
+++ zerotwobot/modules/userinfo.py
@@ -272,7 +272,7 @@ def info(update: Update, context: CallbackContext):
     if user_id not in [bot.id, 777000, 1087968824]:
         userhp = hpmanager(user)
         text += f"\n\n<b>Health:</b> <code>{userhp['earnedhp']}/{userhp['totalhp']}</code>\n[<i>{make_bar(int(userhp['percentage']))} </i>{userhp['percentage']}%]"
-        text += ' [<a href="https://t.me/xmodnews/4">‼🌟</a>]'.format(
+        text += ' [<a href="https://t.me/xmodnews/4">🌟</a>]'.format(
             bot.username,
 
          ) 
