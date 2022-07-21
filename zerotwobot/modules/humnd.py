4d55c04045341e238cddcbbb50801b1cc19fd63a
CUSTOMFAIL <105759853+CUSTOMFAIL@users.noreply.github.com>
2022-07-21 11:55:48 +0530

    Update humnd.py

--- zerotwobot/modules/humnd.py
+++ zerotwobot/modules/humnd.py
@@ -5,7 +5,7 @@ from pyrogram.types import (
     InlineKeyboardMarkup,
     Message,
 )
-from Shikimori import pbot
+from Zerotwobot import pbot
 
 
 @pbot.on_message(filters.command(["cum", "editor"]))
